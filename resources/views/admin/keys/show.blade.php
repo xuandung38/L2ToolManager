e674d45c954acf72ba4ec3674f68998458184dd1
XuanDung38 <xuandung38@gmail.com>
2020-02-12 14:21:22 +0700

    Update show.blade.php

--- resources/views/admin/keys/show.blade.php
+++ resources/views/admin/keys/show.blade.php
@@ -69,7 +69,35 @@
     <script>
         $(function () {
             let dtButtons = $.extend(true, [], $.fn.dataTable.defaults.buttons)
+                    @can('accounts_delete')
+            let deleteButtonTrans = '{{ trans('global.datatables.delete') }}'
+            let deleteButton = {
+                text: deleteButtonTrans,
+                url: "{{ route('admin.accounts.mass_destroy') }}",
+                className: 'btn-danger',
+                action: function (e, dt, node, config) {
+                    var ids = $.map(dt.rows({ selected: true }).nodes(), function (entry) {
+                        return $(entry).data('entry-id')
+                    });
 
+                    if (ids.length === 0) {
+                        alert('{{ trans('global.datatables.zero_selected') }}')
+
+                        return
+                    }
+
+                    if (confirm('{{ trans('global.areYouSure') }}')) {
+                        $.ajax({
+                            headers: {'x-csrf-token': _token},
+                            method: 'POST',
+                            url: config.url,
+                            data: { ids: ids, _method: 'DELETE' }})
+                                .done(function () { location.reload() })
+                    }
+                }
+            }
+            dtButtons.push(deleteButton)
+            @endcan
             $.extend(true, $.fn.dataTable.defaults, {
                 order: [[ 1, 'desc' ]],
                 pageLength: 100,
@@ -77,7 +105,7 @@
             $('.datatable-User:not(.ajaxTable)').DataTable({ buttons: dtButtons })
             $('a[data-toggle="tab"]').on('shown.bs.tab', function(e){
                 $($.fn.dataTable.tables(true)).DataTable()
-                    .columns.adjust();
+                        .columns.adjust();
             });
         })
 
